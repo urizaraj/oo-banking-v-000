fd3f607fb3e241c54356decc2d7ec6ada5d74d25
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 22:28:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,11 +14,19 @@ class Transfer
 
   def execute_transaction
     if !self.sender.valid?
+      self.status = 'rejected'
       return "Transaction rejected. Please check your account balance."
     elsif self.status == 'pending'
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = 'complete'
     end
+  end
+
+  def reverse_transfer
+    if self.status == 'complete'
+      self.sender.balance += self.amount
+      self.receiver.balance -= self.amount
+      self.status = 'reversed'
   end
 end
